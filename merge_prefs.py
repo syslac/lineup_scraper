9eaae66c33a178337d70ca59eca6b741d32ac6f0
syslac <lmezzalira@gmail.com>
2022-12-10 19:04:22 +0100

    Added extra script to sync votes cast on an old list with a new list of bands, freshly downloaded

---
+++ merge_prefs.py
@@ -0,0 +1,33 @@
+from io import StringIO, BytesIO
+import os
+
+try:
+    prefs = open('preferences.csv', 'r')
+    pres = open('presences.csv', 'r')
+
+    prefs_matrix = {}
+    header = prefs.readline()
+    people = [] 
+    for person in header.split(';'):
+        if (person != ''):
+            people.append(person)
+    for band_line in prefs:
+        band_line = band_line.strip()
+        votes = band_line.split(';')
+        prefs_matrix[votes[0]] = votes[1:]
+
+    final_band_list = []
+    for band in pres:
+        final_band_list.append((band.split(';'))[0])
+
+    merged_result = open('merged_preferences.csv', 'w')
+    header = header.strip()
+    print(header, file = merged_result)
+    for band in final_band_list:
+        try:
+            print(band + ';' + ';'.join(prefs_matrix[band]),file = merged_result)
+        except KeyError:
+            print(band + ';' + ';'*header.count(), file = merged_result)
+
+except IOError:
+    exit('file not found')
